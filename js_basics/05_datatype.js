5cc7076449403dedea8a25b2da827972d134b12e
Abhi <kadamabhishek12318@gmail.com>
2024-07-13 11:35:45 +0000

    datatypes

--- js_basics/05_datatype.js
+++ js_basics/05_datatype.js
@@ -16,12 +16,43 @@ const studId= Symbol('123')
 
 let no=null
 
+let type=true
+
 let text=
 
 
-console.log(typeof nos);
-console.log(typeof arr);
-console.log(typeof id);
-console.log(typeof studId);
-console.log(typeof no);
-console.log(typeof text);
+
+// console.log(typeof nos);
+// console.log(typeof arr);
+// console.log(typeof id);
+// console.log(typeof studId);
+// console.log(typeof no);
+// console.log(typeof text);
+// console.log(typeof type)
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*  Stack and Heap  */
+
+user1= "codelearn.com "
+
+let user2=user1
+user2= "learncode.com "
+
+console.log(user1);
+console.log(user2);
+
+//heap changes data for two if any change is made in one var
+
+let ownerone={
+
+    email:"owner1googledotcom"
+}
+
+
+let owner2=ownerone
+owner2.email="owner2googledotcom"
+
+console.log(ownerone);
+console.log(owner2);
