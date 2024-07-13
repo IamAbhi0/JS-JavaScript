bd93fdb28c3a24dff25ce6409b5e2d89b5d5e4da
Abhi <kadamabhishek12318@gmail.com>
2024-07-13 10:20:06 +0000

    data types in js

--- js_basics/04_comparison.js
+++ js_basics/04_comparison.js
@@ -1,6 +1,13 @@
 console.log("Helloo Worldd")
 
-console.log(2!=1);
-console.log(2==1);
-console.log(4>=1);
-console.log("js-hindi")
+// console.log(2!=1);
+// console.log(2==1);
+// console.log(4>=1);
+// console.log("js-hindi")
+
+let array=["js", "hindi" ,"by" , "hitesh"]
+
+let no=symbol('123')
+
+console.log(typeof array);
+console.log(array);
---
+++ js_basics/05_datatype.js
@@ -0,0 +1,27 @@
+/* Primitive */
+
+// Number,String,Boolean,Symbol,NUll,Undefined
+
+/* Non-Primitive*/
+
+// Array,Object,Function
+
+let nos=123
+
+let arr=["JS","by","hitesh"]
+
+const id= Symbol('123')
+
+const studId= Symbol('123')
+
+let no=null
+
+let text=
+
+
+console.log(typeof nos);
+console.log(typeof arr);
+console.log(typeof id);
+console.log(typeof studId);
+console.log(typeof no);
+console.log(typeof text);
