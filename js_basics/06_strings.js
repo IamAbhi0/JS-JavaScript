00f2280468909d563e6db6023c5dadeb21d5783d
Abhi <kadamabhishek12318@gmail.com>
2024-07-13 11:36:00 +0000

    strings

---
+++ js_basics/06_strings.js
@@ -0,0 +1,12 @@
+let nav="hitesh "
+let repo="55"
+
+console.log(nav+repo);
+
+console.log(`hii my name is ${nav} my repo count is ${repo}!`);
+
+let name= new String(`Hitesh`);
+
+console.log(name.charAt(3));
+
+console.log(name.length);
