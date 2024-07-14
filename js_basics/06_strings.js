b8234be14acf60eced7b15f59bd917eef0d3aca4
Abhi <kadamabhishek12318@gmail.com>
2024-07-14 11:04:40 +0000

    strings

--- js_basics/06_strings.js
+++ js_basics/06_strings.js
@@ -10,3 +10,30 @@ let name= new String(`Hitesh`);
 console.log(name.charAt(3));
 
 console.log(name.length);
+
+/* padStart */
+/* slice = The letters/word to show*/
+
+
+const phno='9021310041'
+const last3digits=phno.slice(-3);
+const phnoo=last3digits.padStart(phno.length,'*')
+console.log(phnoo);
+
+const fullNumber = '2034399002125581';
+const last4Digits = fullNumber.slice(-4);
+const maskedNumber = last4Digits.padStart(fullNumber.length, '*');
+
+console.log(maskedNumber);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+const loginName='Abhinav'
+const pass=( loginName.substring('-3')  )
+
+console.log(loginName.substring(3));
+console.log(pass);  
+
+
---
+++ js_basics/07_noandmaths.js
@@ -0,0 +1,20 @@
+const score=700
+
+let newscore=String('score')
+
+console.log(typeof score);
+console.log(typeof newscore);
+
+const value=169
+
+console.log(value);
+console.log(value.toFixed(1));
+
+const value1=189.8978
+
+console.log(value1.toPrecision(3));
+console.log(value1.toPrecision(4));
+
+const hundreds=10000000
+
+console.log(hundreds.toLocaleString('en-IN'));
