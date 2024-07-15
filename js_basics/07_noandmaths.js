8816ea77fc13cb52788096896b6e2c7c8c502ce6
Abhi <kadamabhishek12318@gmail.com>
2024-07-15 10:04:12 +0000

    Math

--- js_basics/07_noandmaths.js
+++ js_basics/07_noandmaths.js
@@ -2,19 +2,41 @@ const score=700
 
 let newscore=String('score')
 
-console.log(typeof score);
-console.log(typeof newscore);
+// console.log(typeof score);
+// console.log(typeof newscore);
 
 const value=169
 
-console.log(value);
-console.log(value.toFixed(1));
+// console.log(value);
+// console.log(value.toFixed(1));
 
 const value1=189.8978
 
-console.log(value1.toPrecision(3));
-console.log(value1.toPrecision(4));
+// console.log(value1.toPrecision(3));
+// console.log(value1.toPrecision(4));
 
 const hundreds=10000000
 
 console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Math.max(2,4,6,8,10));
+// console.log(Math.min(2,4,6,8,10));
+// console.log(Math.round(4.7));
+// console.log(Math.abs(-75));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()+1 );
+console.log( Math.floor (Math.random( )*10) +1 );
+console.log(Math.random(1,6) );
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random() * (max-min + 1)+ min));
+console.log(Math.floor(Math.random()*10) + 10);
+
+const minv=1
+const maxv=6
+
+console.log(Math.floor(Math.random()*(maxv-minv +1)+1));
