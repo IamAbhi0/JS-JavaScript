ccc9881e52fc5f0184bcbeb7a3a268fc367be50e
Abhi <kadamabhishek12318@gmail.com>
2024-07-15 10:50:35 +0000

    Date & Time

---
+++ js_basics/08_DateandTime.js
@@ -0,0 +1,25 @@
+let date= new Date()
+let todaydate=String(date)
+// console.log(todaydate);
+
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+
+// console.log(date.toLocaleDateString())
+
+let mytime=Date.now()
+
+console.log(mytime);
+console.log(date.getTime());
+
+let newdatet= new Date()
+
+console.log(newdatet.toLocaleDateString()) 
+
+console.log(newdatet.getDay());
+console.log(newdatet.getTime());
+
+// console.log( ${Date.getDay} the time is ${Date.getTime}  );
