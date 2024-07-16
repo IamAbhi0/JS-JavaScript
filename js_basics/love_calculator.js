b0c7ce20487d751c81fa67a478b4d3e01acbbcc3
Abhi <kadamabhishek12318@gmail.com>
2024-07-16 11:37:51 +0000

    love_calculator

---
+++ js_basics/love_calculator.js
@@ -0,0 +1,14 @@
+console.log("Welcome to Love Calculator! ");
+
+function yourName(Boy,Girl){
+
+let love=Math.round(Math.random()*100);
+
+console.log (love+"%");
+}
+
+yourName(Arnav,Sanjana);
+
+
+
+// console.log(love);
